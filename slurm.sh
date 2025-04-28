e27adcfd5771ac1fd4011d886f5ccb54514ed583
yoginathsb <30733309+yoginathsb@users.noreply.github.com>
2025-04-27 22:32:00 -0400

    Update slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -15,7 +15,5 @@ cp -R $PWD/data/gmsim_data $MEMBERWORK/
 cp $PWD/data/gmsim_data/treatments.txt $MEMBERWORK/
 cp $PWD/data/gmsim_data/rateData.txt $MEMBERWORK/
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:
-
 export OMP_NUM_THREADS=4
 srun -N 6999 -n 55987 --gpus-per-task=1 --gpu-bind=closest -t 0:30:00 -A xxxxxx -c 7 -S 8 $MEMBERWORK/gmsim_cloner -z 90 -l 6 -k 6 -e 1800 -a 3 
